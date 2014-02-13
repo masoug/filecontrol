75bcd1d658023d0d6fedd0eff547701144fc46b1
Sung-Yee Guo <masoug@cartman.cs.ucsb.edu>
2014-02-12 18:07:53 -0800

    Somehow urls.py was not committed.

--- filecontrol/urls.py
+++ filecontrol/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
 
 urlpatterns = patterns('',
   url(r'^admin/', include(admin.site.urls)),
